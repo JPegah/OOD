09d413886a9a79e9e29b0a5a5d3876496fa80feb
Pegah <pegah@pegah-VPCS13AFG.(none)>
2016-03-06 23:48:10 +0330

    template inheritance added

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,6 +6,8 @@ import play.data.Form;
 
 import views.html.*;
 
+
+
 public class Application extends Controller {
 
     public Result index() {
@@ -14,11 +16,34 @@ public class Application extends Controller {
 
     public Result Courses(){
         return ok(Courses.render("hi"));
-//        return renderTemplate(Courses.scala.html);
     }
 
     public Result LogIn(){
-        return ok(LogIn.render());
+        return ok(LogIn.render(""));
     }
+
+    public Result ChangePass(){
+        return ok(ChangePass.render(""));
+    }
+
+    public Result ForgetPass(){
+        return ok(ForgetPass.render(""));
+    }
+
+    public Result MessageThread(){
+        return ok(MessageThread.render(""));
+    }
+
+    public Result Inbox(){
+        return ok(Inbox.render(""));
+    }
+
+    public Result Upload(Long id){
+        return ok(UploadBox.render(""));
+    }
+
+//    public Result CourseIndex(Long id){
+//        return ok(CourseIndex.courseIndex.render(""));
+//    }
 
 }

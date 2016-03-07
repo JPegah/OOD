f7746cd887cc54e4cb1004064b51d010306ecc64
Pegah <pegah@pegah-VPCS13AFG.(none)>
2016-03-07 15:02:25 +0330

    templates Transferred to scala

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,11 +8,12 @@ import views.html.*;
 
 
 
+
 public class Application extends Controller {
 
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+//    public Result index() {
+//        return ok(index.render("Your new application is ready."));
+//    }
 
     public Result Courses(){
         return ok(Courses.render("hi"));
@@ -46,4 +47,63 @@ public class Application extends Controller {
 //        return ok(CourseIndex.courseIndex.render(""));
 //    }
 
+    public Result Comment(Long id){
+        return ok(views.html.CourseIndex.comment.render(""));
+    }
+
+    public Result newComment(Long id){
+        return ok(views.html.CourseIndex.newComment.render(""));
+    }
+
+    public Result CourseIndex(Long id){
+        return ok(views.html.CourseIndex.courseIndex.render(""));
+    }
+
+    public Result Exams(Long id, Long eId){
+        return ok(views.html.CourseIndex.Exams.render(""));
+    }
+
+    public Result Projects(Long id, Long pId){
+        return ok(views.html.CourseIndex.Projects.render(""));
+    }
+
+    public Result CourseDescription(Long id){
+        return ok(views.html.CourseIndex.StudentView.CourseDesc.render(""));
+    }
+
+    public Result Grades(Long id){
+        return ok(views.html.CourseIndex.StudentView.Grades.render(""));
+    }
+
+    public Result ProjectInd(Long id, Long p_id){
+        return ok(views.html.CourseIndex.StudentView.ProjectInd.render(""));
+    }
+    public Result Resources(Long id){
+        return ok(views.html.CourseIndex.StudentView.Resources.render(""));
+    }
+
+    public Result AddGrade(Long id){
+        return ok(views.html.CourseIndex.ProfView.AddGrade.render(""));
+    }
+
+    public Result EditDescription(Long id){
+        return ok(views.html.CourseIndex.ProfView.EditDescription.render(""));
+    }
+
+    public Result EditGrades(Long id){
+        return ok(views.html.CourseIndex.ProfView.EditGrades.render(""));
+    }
+    public Result EditProject(Long id){
+        return ok(views.html.CourseIndex.ProfView.EditProject.render(""));
+    }
+
+    public Result EditResources(Long id){
+        return ok(views.html.CourseIndex.ProfView.EditResources.render(""));
+    }
+
+    public Result ProjectEdit(Long id, Long p_id){
+        return ok(views.html.CourseIndex.ProfView.ProjectsEdit.render(""));
+    }
+
 }
+

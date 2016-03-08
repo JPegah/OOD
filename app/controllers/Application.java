41c8d581108174170e94596c27a8ed32a6b99246
Pegah <pegah@pegah-VPCS13AFG.(none)>
2016-03-08 16:21:44 +0330

    logic Interface craeted

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -16,31 +16,31 @@ public class Application extends Controller {
 //    }
 
     public Result Courses(){
-        return ok(Courses.render("hi"));
+        return ok(Courses.render("لیست دروس"));
     }
 
     public Result LogIn(){
-        return ok(LogIn.render(""));
+        return ok(LogIn.render("ورود"));
     }
 
     public Result ChangePass(){
-        return ok(ChangePass.render(""));
+        return ok(ChangePass.render("تغییر رمز"));
     }
 
     public Result ForgetPass(){
-        return ok(ForgetPass.render(""));
+        return ok(ForgetPass.render("فراموشی رمز"));
     }
 
     public Result MessageThread(){
-        return ok(MessageThread.render(""));
+        return ok(MessageThread.render("ریسه ی ‍‍پیام"));
     }
 
     public Result Inbox(){
-        return ok(Inbox.render(""));
+        return ok(Inbox.render("صندوق پیام"));
     }
 
     public Result Upload(Long id){
-        return ok(UploadBox.render(""));
+        return ok(UploadBox.render("صندوق بارگذاری"));
     }
 
 //    public Result CourseIndex(Long id){
@@ -48,61 +48,61 @@ public class Application extends Controller {
 //    }
 
     public Result Comment(Long id){
-        return ok(views.html.CourseIndex.comment.render(""));
+        return ok(views.html.CourseIndex.comment.render("مشاهده نظرات"));
     }
 
     public Result newComment(Long id){
-        return ok(views.html.CourseIndex.newComment.render(""));
+        return ok(views.html.CourseIndex.newComment.render("افزودن نظر"));
     }
 
     public Result CourseIndex(Long id){
-        return ok(views.html.CourseIndex.courseIndex.render(""));
+        return ok(views.html.CourseIndex.courseIndex.render("نمایه درس"));
     }
 
     public Result Exams(Long id, Long eId){
-        return ok(views.html.CourseIndex.Exams.render(""));
+        return ok(views.html.CourseIndex.Exams.render("امتحانات"));
     }
 
     public Result Projects(Long id, Long pId){
-        return ok(views.html.CourseIndex.Projects.render(""));
+        return ok(views.html.CourseIndex.Projects.render("پروژه ها"));
     }
 
     public Result CourseDescription(Long id){
-        return ok(views.html.CourseIndex.StudentView.CourseDesc.render(""));
+        return ok(views.html.CourseIndex.StudentView.CourseDesc.render("طرح  درس"));
     }
 
     public Result Grades(Long id){
-        return ok(views.html.CourseIndex.StudentView.Grades.render(""));
+        return ok(views.html.CourseIndex.StudentView.Grades.render("مشاهده نمرات"));
     }
 
     public Result ProjectInd(Long id, Long p_id){
-        return ok(views.html.CourseIndex.StudentView.ProjectInd.render(""));
+        return ok(views.html.CourseIndex.StudentView.ProjectInd.render("نمایه پروژه"));
     }
     public Result Resources(Long id){
-        return ok(views.html.CourseIndex.StudentView.Resources.render(""));
+        return ok(views.html.CourseIndex.StudentView.Resources.render("منابع درس"));
     }
 
     public Result AddGrade(Long id){
-        return ok(views.html.CourseIndex.ProfView.AddGrade.render(""));
+        return ok(views.html.CourseIndex.ProfView.AddGrade.render("افزودن نمره"));
     }
 
     public Result EditDescription(Long id){
-        return ok(views.html.CourseIndex.ProfView.EditDescription.render(""));
+        return ok(views.html.CourseIndex.ProfView.EditDescription.render("ویرایش طرح درس"));
     }
 
     public Result EditGrades(Long id){
-        return ok(views.html.CourseIndex.ProfView.EditGrades.render(""));
+        return ok(views.html.CourseIndex.ProfView.EditGrades.render("ویرایش نمرات"));
     }
     public Result EditProject(Long id){
-        return ok(views.html.CourseIndex.ProfView.EditProject.render(""));
+        return ok(views.html.CourseIndex.ProfView.EditProject.render("ویرایش پروژه"));
     }
 
     public Result EditResources(Long id){
-        return ok(views.html.CourseIndex.ProfView.EditResources.render(""));
+        return ok(views.html.CourseIndex.ProfView.EditResources.render("ویرایش منابع"));
     }
 
     public Result ProjectEdit(Long id, Long p_id){
-        return ok(views.html.CourseIndex.ProfView.ProjectsEdit.render(""));
+        return ok(views.html.CourseIndex.ProfView.ProjectsEdit.render("ویرایش پروژه"));
     }
 
 }
---
+++ app/controllers/LogicInterface.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import LogicClasses.User;
+
+/**
+ * Created by pegah on 3/8/16.
+ */
+public class LogicInterface {
+    public static User getUser(Long id){
+        
+    }
+}
